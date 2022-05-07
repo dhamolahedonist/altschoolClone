55a83477dc8f1e3354184e060b794f4c572c0f8a
Samuel Oduba <samueloduba123@gmail.com>
2022-05-07 03:47:08 -0700

    alt school clone app

---
+++ alt.js
@@ -0,0 +1,28 @@
+const menuToggle = document.querySelector('.menu-btn');
+const links = document.querySelector('.links');
+
+menuToggle.addEventListener('click', function() {
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links');
+    // }
+    // else {
+    //     links.classList.add('show-links');
+    // }
+    links.classList.toggle('show-links');
+});
+
+const navBar = document.getElementById('nav');
+window.addEventListener('scroll', function() {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight) {
+        navBar.classList.add('fixed-nav');
+    }
+    else {
+        navBar.classList.remove('fixed-nav');
+    }
+})
+
+
+const items = document.querySelector('.container p')
+
